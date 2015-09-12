236ce77c9db0507655ad7a7673b8360b80b650e5
Seiichirou Harada <umbrella.jpn@gmail.com>
2015-09-12 12:03:25 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Footballa::Application.config.secret_key_base = '4d0b1943c0cd02fe55edc74711af00053a0f9116a3f6fd3deead5eeab6119b37f3e57a924b541b5819f991c17945e24411e18861eb925cb19e2fcaf208510103'
